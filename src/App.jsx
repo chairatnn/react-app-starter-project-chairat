1db067b2f984ee540ac2712bbeaa2cce693e988f
chairatnn <61372139+chairatnn@users.noreply.github.com>
2025-12-05 12:48:28 +0700

    addNavbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./views/Home";
+import About from "./views/About";
+import Contact from "./views/Contact";
+import Products from "./views/Products";
+import ProductDetail from "./views/ProductDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-4xl">404 - Page Not Found 🥲</h1>
+    </div>
+    ),
+    children: [
+      { path: "/",element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
+      { path: "products", element: <Products /> },
+      { path: "products/:productId", element: <ProductDetail /> },
+    ],
+  },
+]);
+
 export default function App() {
   return (
-    <div className="min-h-screen flex justify-center bg-blue-950">
-      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
-        <h1 className="w-full p-6 bg-amber-100 font-extrabold">
-          React App Starter
-        </h1>
-        <section className="w-full p-5 bg-amber-100 flex">
-          <ul className="list-inside list-disc flex-1">
-            <span className="font-semibold">Tech Stack:</span>
-            <li>Vite</li>
-            <li>React</li>
-            <li>JavaScript</li>
-            <li>Tailwind</li>
-          </ul>
-        </section>
-      </div>
-    </div>
+    <RouterProvider router={router} />
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+    import { Link } from "react-router-dom";
+
+    export default function Navbar(){
+        return (
+        <nav className="bg-teal-500 text-white p-4 shadow-md">
+            <ul className="flex gap-4 justify-center">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                    <Link to="/products">Products</Link>
+                </li>
+            </ul>
+        </nav>
+        )
+}
